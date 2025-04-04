8ec59ae531e37b5c53794b446d6738522ae36600
ductaip <dtai858@gmail.com>
2025-04-04 08:20:12 +0700

    refactor and rm class-validator

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,48 +1,47 @@
-import { plainToInstance } from 'class-transformer';
-import { IsString, validateSync } from 'class-validator';
-import fs from 'fs';
-import path from 'path';
-import { config } from 'dotenv';
+import { plainToInstance } from 'class-transformer'
+import fs from 'fs'
+import path from 'path'
+import { config } from 'dotenv'
 
 config({
   path: '.env',
-});
+})
 // check that exist env file
 if (!fs.existsSync(path.resolve('.env'))) {
-  console.log('Không tìm thấy file .env');
-  process.exit(1);
+  console.log('Không tìm thấy file .env')
+  process.exit(1)
 }
 
 class ConfigSchema {
   @IsString()
-  DATABASE_URL: string;
+  DATABASE_URL: string
   @IsString()
-  ACCESS_TOKEN_SECRET: string;
+  ACCESS_TOKEN_SECRET: string
   @IsString()
-  ACCESS_TOKEN_EXPIRES_IN: string;
+  ACCESS_TOKEN_EXPIRES_IN: string
   @IsString()
-  REFRESH_TOKEN_SECRET: string;
+  REFRESH_TOKEN_SECRET: string
   @IsString()
-  REFRESH_TOKEN_EXPIRES_IN: string;
+  REFRESH_TOKEN_EXPIRES_IN: string
   @IsString()
-  SECRET_API_KEY: string;
+  SECRET_API_KEY: string
 }
 const configServer = plainToInstance(ConfigSchema, process.env, {
   enableImplicitConversion: true,
-});
-const errorArray = validateSync(configServer);
+})
+const errorArray = validateSync(configServer)
 
 if (errorArray.length > 0) {
-  console.log('Các giá trị khai báo trong file .env không hợp lệ');
+  console.log('Các giá trị khai báo trong file .env không hợp lệ')
   const errors = errorArray.map((eItem) => {
     return {
       property: eItem.property,
       constraints: eItem.constraints,
       value: eItem.value,
-    };
-  });
-  throw errors;
+    }
+  })
+  throw errors
 }
-const envConfig = configServer;
+const envConfig = configServer
 
-export default envConfig;
+export default envConfig
