d1588bb0dc3998282ee9b847a633195b2e679290
ductaip <dtai858@gmail.com>
2025-04-05 15:08:06 +0700

    Repository Pattern: config new model for auth and add pattern

--- src/shared/constants/auth.constant.ts
+++ src/shared/constants/auth.constant.ts
@@ -1,17 +1,22 @@
-export const REQUEST_USER_KEY = 'user';
+export const REQUEST_USER_KEY = 'user'
 
 export const AuthType = {
   Bearer: 'Bearer',
   None: 'None',
   APIKey: 'ApiKey',
-} as const;
+} as const
 
-export type AuthTypeType = (typeof AuthType)[keyof typeof AuthType];
+export type AuthTypeType = (typeof AuthType)[keyof typeof AuthType]
 
 export const ConditionGuard = {
   And: 'and',
   Or: 'or',
-} as const;
+} as const
 
-export type ConditionGuardType =
-  (typeof ConditionGuard)[keyof typeof ConditionGuard];
+export type ConditionGuardType = (typeof ConditionGuard)[keyof typeof ConditionGuard]
+
+export const UserStatus = {
+  ACTIVE: 'ACTIVE',
+  INACTIVE: 'INACTIVE',
+  BLOCKED: 'BLOCKED',
+} as const
